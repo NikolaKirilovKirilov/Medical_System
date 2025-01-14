184796eb4a7e710f6a7a4843ce61a796bb755977
Nikola Kirilov <nikolakirilov145@gmail.com>
2025-01-14 20:46:33 +0200

    Some changes to the window sizes

--- medicalsystem/src/main/java/ui/doctorView.java
+++ medicalsystem/src/main/java/ui/doctorView.java
@@ -15,15 +15,12 @@ public class doctorView extends JFrame implements ActionListener{
 	public doctorView() {
 		this.setTitle("Hello Doctor");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		this.setResizable(false);
-		this.setSize(800, 900);
 		this.setVisible(true);
 		
 		ImageIcon icon = new ImageIcon("Medical_emblem.png");
 		this.setIconImage(icon.getImage());
-		
-		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-		this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
 	}
 	
 	@Override
